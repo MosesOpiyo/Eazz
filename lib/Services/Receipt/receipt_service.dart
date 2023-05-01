5b159c4febe2c244ef9049854784ac212c2f9bfb
MosesOpiyo <mosesopiyo54@gmail.com>
2023-05-01 22:19:07 +0300

    Endpoint servicing

--- lib/Services/Receipt/receipt_service.dart
+++ lib/Services/Receipt/receipt_service.dart
@@ -1,18 +1,24 @@
 // ignore_for_file: avoid_print, prefer_interpolation_to_compose_strings
 
 import 'package:eazz/Models/Receipts/receipt_models.dart';
+import 'package:eazz/Models/Receipts/statement_model_response.dart';
 import 'package:eazz/Models/Receipts/single_receipt_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:eazz/Constants/constants.dart';
 import 'dart:convert' as cnv;
+import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ReceiptService {
   var client = http.Client();
   Future postReceipt(result) async {
+    String? token;
+    final prefs = await SharedPreferences.getInstance();
+    token = prefs.getString('token');
     Map<String, String> requestHeaders = {
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Charset': 'utf-8'
+      'Content-Type': 'application/json',
+      'Charset': 'utf-8',
+      HttpHeaders.authorizationHeader: "Token $token"
     };
     var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.receipt);
 
@@ -56,5 +62,24 @@ class ReceiptService {
       headers: requestHeaders,
     );
     return SingleReceiptResponseModel.fromJson(cnv.jsonDecode(response.body));
+  }
+
+  Future<List<StatementsResponseModel>> getStatements() async {
+    String? token;
+    final prefs = await SharedPreferences.getInstance();
+    token = prefs.getString('token');
+    Map<String, String> requestHeaders = {'Authorization': 'Token $token'};
+    var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.statements);
+
+    var response = await http.get(
+      url,
+      headers: requestHeaders,
+    );
+    List<dynamic> body = cnv.jsonDecode(response.body);
+    var statement = body
+        .map((dynamic item) => StatementsResponseModel.fromJson(item))
+        .toList();
+
+    return statement;
   }
 }
